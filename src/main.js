4ada5c0830795ed8d5331819938bd906c282d5d4
Freedauss Epoundor TANDA <91498943+epoundor@users.noreply.github.com>
2023-07-06 07:43:27 +0000

    fix(front): remove all import

--- src/main.js
+++ src/main.js
@@ -36,25 +36,6 @@ class Projectile extends Player{
     }
 }
 
-// ================== Projectile ================
-
-class Projectile extends Player{
-    constructor(x,y,radius,color,velocity){
-        super()
-        this.x=x;
-        this.y=y;
-        this.radius=radius;
-        this.color=color
-        this.velocity=velocity
-    }
-
-    update(){
-        this.draw()
-        this.x+=this.velocity.x;
-        this.y+=this.velocity.y;
-    }
-}
-
 // ================== Enemy ================
 class Enemy extends Projectile{
     
